3efefabe71227b52f7c2bfbbe97f9edbf8295cea
Sean Middleditch <sean@middleditch.us>
2011-03-27 15:42:47 -0700

    Fix MSVC warning.

--- gamepad.c
+++ gamepad.c
@@ -130,8 +130,8 @@ void GamepadSetRumble(GAMEPAD_DEVICE gamepad, float left, float right) {
 	if ((STATE[gamepad].flags & FLAG_RUMBLE) != 0) {
 		XINPUT_VIBRATION vib;
 		ZeroMemory(&vib, sizeof(vib));
-		vib.wLeftMotorSpeed = left * 65535;
-		vib.wRightMotorSpeed = right * 65535;
+		vib.wLeftMotorSpeed = (WORD)(left * 65535);
+		vib.wRightMotorSpeed = (WORD)(right * 65535);
 		XInputSetState(gamepad, &vib);
 	}
 }
